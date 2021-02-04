8eea4d856aa6a0629d9344bc63483b04ed463bb2
Gabriel <gabriel.camarac@gmail.com>
2021-02-04 17:50:25 -0300

    Second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { FirstComponentComponent } from './libs/first-component/first-component.component';
+import { SecondComponentComponent } from './libs/second-component/second-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FirstComponentComponent
+    FirstComponentComponent,
+    SecondComponentComponent
   ],
   imports: [
     BrowserModule
