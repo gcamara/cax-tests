cfc48a79a603295a2bac28f6f7f30f90f3568c8e
Gabriel <gabriel.camarac@gmail.com>
2021-02-04 18:15:06 -0300

    ..

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { FirstComponentComponent } from './libs/first-component/first-component.component';
 import { SecondComponentComponent } from './libs/second-component/second-component.component';
 import { ThirdComponentComponent } from './libs/third-component/third-component.component';
+import { FourthComponentComponent } from './libs/fourth-component/fourth-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FirstComponentComponent,
     SecondComponentComponent,
-    ThirdComponentComponent
+    ThirdComponentComponent,
+    FourthComponentComponent
   ],
   imports: [
     BrowserModule
