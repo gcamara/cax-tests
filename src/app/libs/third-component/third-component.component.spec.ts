c60a1ca44309e2ddf53f97a4aebdde98daf461e2
Gabriel <gabriel.camarac@gmail.com>
2021-02-04 18:04:38 -0300

    .

--- src/app/libs/third-component/third-component.component.spec.ts
+++ src/app/libs/third-component/third-component.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ThirdComponentComponent } from './third-component.component';
+
 
 describe('ThirdComponentComponent', () => {
   let component: ThirdComponentComponent;
@@ -21,5 +21,6 @@ describe('ThirdComponentComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+    console.log('teste');
   });
 });
