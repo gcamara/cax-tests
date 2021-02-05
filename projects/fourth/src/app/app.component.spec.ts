525150d2f0f8c68c31924266328149c510a280e1
Gabriel <gabriel.camarac@gmail.com>
2021-02-04 23:37:44 -0300

    big diff

--- projects/fourth/src/app/app.component.spec.ts
+++ projects/fourth/src/app/app.component.spec.ts
@@ -16,17 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'second'`, () => {
+  it(`should have as title 'fourth'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('second');
+    expect(app.title).toEqual('fourth');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('second app is running!');
-    console.log('abc');
+    expect(compiled.querySelector('.content span').textContent).toContain('fourth app is running!');
   });
 });
