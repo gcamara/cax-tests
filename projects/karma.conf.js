6e5fa3fa5a28c02996b8d7e32c33edecb03eef7f
Gabriel <gabriel.camarac@gmail.com>
2021-02-04 21:44:44 -0300

    Adding chromium

--- projects/karma.conf.js
+++ projects/karma.conf.js
@@ -1,6 +1,10 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
+const chromium = require('chromium');
+
+process.env.CHROME_BIN = chromium.path;
+
 module.exports = function (config) {
   config.set({
     basePath: '',
